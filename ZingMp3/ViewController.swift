de7e2e0ca2da397e0ad3304943490d6705e4c152
techmaster <techmaster@techmasters-iMac-3.local>
2017-01-05 15:06:30 +0700

    First commit

--- ZingMp3/ViewController.swift
+++ ZingMp3/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgLogo: UIImageView!
+    @IBOutlet weak var lblText: UILabel!
+    @IBOutlet weak var lblName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imgLogo.alpha = 0
+        lblText.alpha = 0
+        lblName.alpha = 0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UIView.animate(withDuration: 4, animations: { 
+            self.imgLogo.alpha = 1
+            }) { (false) in
+                UIView.animate(withDuration: 4, animations: { 
+                    self.lblText.alpha = 1
+                    self.lblText.center = CGPoint(x: self.imgLogo.center.x, y: 100)
+                    }, completion: { (false) in
+                        UIView.animate(withDuration: 4, animations: { 
+                            self.lblName.alpha = 1
+                            self.lblName.center = CGPoint(x: self.imgLogo.center.x, y: UIScreen.main.bounds.height - 100)
+                        })
+                })
+        }
     }
-
 
 }
 
